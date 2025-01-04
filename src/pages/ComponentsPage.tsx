961555db1614a2a73c754b29d4d7570acee9b63c
Louisse <louissevale@icloud.com>
2025-01-04 16:43:04 +0800

    feat(pages): create Components page

---
+++ src/pages/ComponentsPage.tsx
@@ -0,0 +1,12 @@
+import { ButtonComponent, TextInputComponent } from "../components";
+
+function ComponentsPage() {
+	return (
+		<div className="space-y-12">
+			<ButtonComponent />
+			<TextInputComponent />
+		</div>
+	);
+}
+
+export default ComponentsPage;
