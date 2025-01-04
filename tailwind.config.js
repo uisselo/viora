9b088810d8733754d55a3a177a84a36a16127c42
Louisse <louissevale@icloud.com>
2025-01-04 16:38:03 +0800

    chore: modified a few formatting/linting rules, modified tailwind config, and installed heroicons, classname, lodash-es, and react-router

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#A45B94",
+				secondary: "#EDDEEA",
+			},
+		},
+		fontFamily: {
+			sans: ["Urbanist"],
+		},
+	},
+	plugins: [],
+};
