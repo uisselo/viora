5baac76e49b9a68ea5e4df8efc62610fde254fd4
uisselo <louissemevale@gmail.com>
2025-01-05 16:51:06 +0800

    chore: modify formatter, installed useHooks, added a custom screen width

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			screens: {
+				"custom-lg": "1200px"
+			},
 			colors: {
 				primary: "#A45B94",
 				secondary: "#EDDEEA",
