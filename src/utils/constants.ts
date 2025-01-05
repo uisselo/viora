6ee6f5a0ab780010a3a4a46cf1b001a03b1eeeff
uisselo <louissemevale@gmail.com>
2025-01-05 16:59:08 +0800

    feat(utils): create constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+export const NAV_ITEMS = [
+  "Categories",
+  "Bestsellers",
+  "New Arrivals",
+  "On Sale",
+];
+
+export const ABOUT_US_LINKS = [
+  "About Viora",
+  "Privacy Policy",
+  "Terms of Use",
+];
+
+export const HELP_LINKS = [
+  "Delivery and Shipping",
+  "Payment Options",
+  "Returns",
+  "Order Status",
+  "Contact Us",
+];
