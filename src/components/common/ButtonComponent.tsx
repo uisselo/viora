55b03d0536cf1a5b67079714fa56a18519e06024
uisselo <louissemevale@gmail.com>
2025-01-05 16:53:22 +0800

    refactor(ButtonComponent): fix tab spacing and rename constants

--- src/components/common/ButtonComponent.tsx
+++ src/components/common/ButtonComponent.tsx
@@ -2,69 +2,68 @@ import classNames from "classnames";
 import type { HeroIconProp } from "../../utils";
 
 type Props = {
-	text?: string;
-	variant?: "primary" | "outline" | "link";
-	size?: "xs" | "sm" | "base" | "lg" | "xl";
-	icon?: HeroIconProp;
-	iconPosition?: "trailing" | "leading";
-	isFull?: boolean;
-	hideText?: boolean;
+  text?: string;
+  variant?: "primary" | "outline" | "link";
+  size?: "xs" | "sm" | "base" | "lg" | "xl";
+  icon?: HeroIconProp;
+  iconPosition?: "trailing" | "leading";
+  isFull?: boolean;
+  hideText?: boolean;
+  onClick?: () => void;
 };
 
-const mapVariant = {
-	primary: "text-white bg-primary",
-	outline: "text-primary bg-none border-[1.5px] border-primary",
-	link: "text-primary bg-none px-0 py-0",
+const MAP_VARIANT = {
+  primary: "text-white bg-primary",
+  outline: "text-primary bg-none border-[1.5px] border-primary",
+  link: "text-primary bg-none px-0 py-0",
 };
 
-const mapSize = {
-	xs: "text-xs px-2 py-1.5 gap-x-1.5",
-	sm: "text-sm px-3 py-2 gap-x-1.5",
-	base: "px-4 py-3 gap-x-2",
-	lg: "text-lg px-4 py-3 gap-x-2",
-	xl: "text-xl px-4 py-3 gap-x-2",
+const MAP_SIZE = {
+  xs: "text-xs px-2 py-1.5 gap-1.5",
+  sm: "text-sm px-3 py-2 gap-1.5",
+  base: "px-4 py-3 gap-2",
+  lg: "text-lg px-4 py-3 gap-2",
+  xl: "text-xl px-4 py-3 gap-2",
 };
 
-const mapIconSize = {
-	xs: "w-3 h-3",
-	sm: "w-4 h-4",
-	base: "w-[18px] h-[18px]",
-	lg: "w-5 h-5",
-	xl: "w-6 h-6",
+const MAP_ICON_SIZE = {
+  xs: "w-3 h-3",
+  sm: "w-4 h-4",
+  base: "w-[18px] h-[18px]",
+  lg: "w-5 h-5",
+  xl: "w-6 h-6",
 };
 
 function ButtonComponent(props: Props) {
-	const {
-		text = "Button",
-		variant = "primary",
-		size = "base",
-		icon: Icon,
-		iconPosition = "leading",
-		isFull,
-		hideText,
-	} = props;
+  const {
+    text = "Button",
+    variant = "primary",
+    size = "base",
+    icon: Icon,
+    iconPosition = "leading",
+    isFull,
+    hideText,
+    onClick,
+  } = props;
 
-	return (
-		<button
-			className={classNames(
-				"flex font-semibold rounded items-center",
-				isFull ? "w-full" : "w-max",
-				mapVariant[variant],
-				mapSize[size],
-				iconPosition === "trailing" ? "flex-row-reverse" : "",
-			)}
-		>
-			{!hideText && text}
-			{Icon && (
-				<Icon
-					className={classNames(
-						"stroke-current stroke-2",
-						mapIconSize[size],
-					)}
-				/>
-			)}
-		</button>
-	);
+  const CLASSES = {
+    CONTAINER: classNames(
+      "flex font-semibold rounded justify-center items-center cursor-pointer",
+      isFull ? "w-full" : "w-max",
+      MAP_SIZE[size],
+      MAP_VARIANT[variant],
+      { "!p-0": variant === "link" },
+      { "flex-row-reverse": iconPosition === "trailing" },
+    ),
+    ICON: classNames("stroke-current stroke-1.5", MAP_ICON_SIZE[size]),
+  };
+
+  return (
+    <button className={CLASSES.CONTAINER} onClick={onClick}>
+      {!hideText && text}
+      {Icon && <Icon className={CLASSES.ICON} />}
+    </button>
+  );
 }
 
 export default ButtonComponent;
