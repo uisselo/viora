a9caf0ef9e01ec0f492cd34e7cb0e9c2ba104c10
uisselo <louissemevale@gmail.com>
2025-01-06 19:31:11 +0800

    style(LayoutComponent): remove unnecessary styles

--- src/components/layout/LayoutComponent.tsx
+++ src/components/layout/LayoutComponent.tsx
@@ -3,16 +3,10 @@ import FooterComponent from "./FooterComponent";
 import HeaderComponent from "./HeaderComponent";
 
 function LayoutComponent() {
-  const CLASSES = {
-    CONTAINER:
-      "relative flex flex-col items-center justify-center gap-8 md:gap-12 lg:gap-16",
-    MAIN: "container grid h-screen grid-cols-4 gap-4 md:grid-cols-12 lg:gap-5",
-  };
-
   return (
-    <div className={CLASSES.CONTAINER}>
+    <div className="relative flex flex-col items-center gap-8 md:gap-12 lg:gap-16">
       <HeaderComponent />
-      <main className={CLASSES.MAIN}>
+      <main className="container">
         <Outlet />
       </main>
       <FooterComponent />
