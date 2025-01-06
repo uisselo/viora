b289d2ed8091e35b44c77a97855df12d6adb25f6
uisselo <louissemevale@gmail.com>
2025-01-06 19:35:25 +0800

    feat(ComponentsPage): display newly created components

--- src/pages/ComponentsPage.tsx
+++ src/pages/ComponentsPage.tsx
@@ -1,15 +1,37 @@
 import {
   ButtonComponent,
   NavItemComponent,
+  ProductItemComponent,
+  QuantityComponent,
   TextInputComponent,
 } from "../components";
+import { useProductQueries } from "../store";
 
 function ComponentsPage() {
+  const { beautyData } = useProductQueries();
+
   return (
-    <div className="flex flex-col col-span-6 gap-12">
-      <ButtonComponent />
-      <TextInputComponent />
-      <NavItemComponent />
+    <div className="flex flex-col gap-12">
+      <div className="grid grid-cols-4 gap-4 md:grid-cols-12 lg:gap-5">
+        <div className="flex flex-col col-span-4 gap-12 md:col-span-6">
+          <ButtonComponent />
+          <TextInputComponent />
+          <NavItemComponent />
+          <QuantityComponent value={20} isDisplayOnly />
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-4 md:grid-cols-12 lg:gap-5">
+        <div className="col-span-2 gap-12 md:col-span-4 lg:col-span-3">
+          {beautyData && <ProductItemComponent data={beautyData[0]} />}
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-4 md:grid-cols-12 lg:gap-5">
+        <div className="flex flex-col col-span-4 gap-12 md:col-span-10 lg:col-span-8">
+          {beautyData && (
+            <ProductItemComponent data={beautyData[0]} display="shopping bag" />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
