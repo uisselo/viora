8780916d5687f15da527320925aa3b6ed9ede707
uisselo <louissemevale@gmail.com>
2025-01-06 19:29:49 +0800

    refactor(FooterComponent): map sections

--- src/components/layout/FooterComponent.tsx
+++ src/components/layout/FooterComponent.tsx
@@ -3,25 +3,30 @@ import VioraLogo from "../../assets/viora_logo.svg";
 import { ABOUT_US_LINKS, HELP_LINKS } from "../../utils";
 import { ButtonComponent } from "../common";
 
-function FooterComponent() {
-  const CLASSES = {
-    CONTAINER: "flex justify-center w-full border-t",
-    CONTENT:
-      "container flex flex-col md:grid md:grid-cols-12 gap-4 lg:gap-5 py-4 md:py-8",
-    CONTENT_COL_1: "md:col-span-6",
-    CONTENT_COL_2:
-      "flex flex-col gap-4 md:grid md:grid-cols-4 lg:gap-5 md:col-span-6",
-    SECTION: "flex flex-col gap-3 md:col-span-2",
-    SECTION_TITLE: "text-sm tracking-wider uppercase",
-    SECTION_LINKS: "flex flex-wrap gap-4 md:flex-col md:gap-2",
-  };
+const CLASSES = {
+  CONTAINER: "flex justify-center w-full border-t",
+  CONTENT:
+    "container flex flex-col md:grid md:grid-cols-12 gap-4 lg:gap-5 py-4 md:py-8",
+  CONTENT_COL_1: "md:col-span-6",
+  CONTENT_COL_2:
+    "flex flex-col gap-4 md:grid md:grid-cols-4 lg:gap-5 md:col-span-6",
+  SECTION: "flex flex-col gap-3 md:col-span-2",
+  SECTION_TITLE: "text-sm tracking-wider uppercase",
+  SECTION_LINKS: "flex flex-wrap gap-4 md:flex-col md:gap-2",
+};
 
-  const renderSection = (title: string, links: string[]) => (
-    <div className={CLASSES.SECTION}>
+const SECTIONS = [
+  { title: "About Us", links: ABOUT_US_LINKS },
+  { title: "Help", links: HELP_LINKS },
+];
+
+function FooterComponent() {
+  const renderSection = (title: string, links: string[], key: string) => (
+    <div key={key} className={CLASSES.SECTION}>
       <p className={CLASSES.SECTION_TITLE}>{title}</p>
       <ul className={CLASSES.SECTION_LINKS}>
-        {links.map((item, index) => (
-          <li key={index.toString()}>
+        {links.map((item) => (
+          <li key={item}>
             <ButtonComponent text={item} variant="link" size="sm" />
           </li>
         ))}
@@ -38,8 +43,9 @@ function FooterComponent() {
           </Link>
         </div>
         <div className={CLASSES.CONTENT_COL_2}>
-          {renderSection("About Us", ABOUT_US_LINKS)}
-          {renderSection("Help", HELP_LINKS)}
+          {SECTIONS.map((section) =>
+            renderSection(section.title, section.links, section.title),
+          )}
         </div>
       </div>
     </footer>
