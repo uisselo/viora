bc166ba6b6f26d9954e93a28e77d89122c5c081b
uisselo <louissemevale@gmail.com>
2025-01-07 02:25:54 +0800

    feat(QuantityComponent): add onChange prop and reformat code

--- src/components/common/QuantityComponent.tsx
+++ src/components/common/QuantityComponent.tsx
@@ -1,48 +1,62 @@
 import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
-import { forwardRef, useState } from "react";
+import classNames from "classnames";
+import { forwardRef, useEffect, useState } from "react";
 import ButtonIconComponent from "./ButtonIconComponent";
 
 type Props = {
   value?: number;
   limit?: number;
   isDisplayOnly?: boolean;
+  onChange?: (value: number) => void;
 };
 
 const CLASSES = {
-  CONTAINER: "flex items-center px-2 w-max bg-white border border-gray-300",
-  NUMBER_DISPLAY_CONTAINER:
-    "flex items-center justify-center w-full h-full max-w-8 py-2 min-w-8 aspect-ratio",
-  NUMBER_DISPLAY_TEXT: "font-medium text-center break-words",
+  CONTAINER: "flex items-center justify-center",
+  BORDER: "border border-gray-300",
+  TEXT: "text-sm font-medium text-center break-words md:text-base",
 };
 
-function NumberDisplay({ data }: { data: number }) {
+const applyBorderedContainer = (...classes: string[]) =>
+  classNames(...classes, CLASSES.CONTAINER, CLASSES.BORDER);
+
+function DefaultDisplay(props: Props) {
   return (
-    <div className={CLASSES.NUMBER_DISPLAY_CONTAINER}>
-      <p className={CLASSES.NUMBER_DISPLAY_TEXT}>{data}</p>
+    <div className={applyBorderedContainer("w-9 h-9")}>
+      <p className={CLASSES.TEXT}>{props.value}</p>
     </div>
   );
 }
 
-const QuantityComponent = forwardRef<HTMLDivElement, Props>((props, ref) => {
-  const { value, limit, isDisplayOnly } = props;
-
+const AdjustmentDisplay = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const { limit = 10, onChange } = props;
   const [number, setNumber] = useState<number>(0);
 
   const onClickMinus = () => setNumber((prev) => Math.max(0, prev - 1));
-  const onClickPlus = () =>
-    setNumber((prev) => Math.min(limit || 10, prev + 1));
+  const onClickPlus = () => setNumber((prev) => Math.min(limit, prev + 1));
+
+  useEffect(() => {
+    if (onChange) {
+      onChange(number);
+    }
+  }, [number, onChange]);
 
   return (
-    <div ref={ref} className={CLASSES.CONTAINER}>
-      {!isDisplayOnly && (
-        <ButtonIconComponent icon={MinusIcon} onClick={onClickMinus} />
-      )}
-      <NumberDisplay data={isDisplayOnly && value ? value : number} />
-      {!isDisplayOnly && (
-        <ButtonIconComponent icon={PlusIcon} onClick={onClickPlus} />
-      )}
+    <div ref={ref} className={applyBorderedContainer("px-1")}>
+      <ButtonIconComponent icon={MinusIcon} onClick={onClickMinus} />
+      <div className={classNames("w-9 h-9", CLASSES.CONTAINER)}>
+        <p className={CLASSES.TEXT}>{number}</p>
+      </div>
+      <ButtonIconComponent icon={PlusIcon} onClick={onClickPlus} />
     </div>
   );
+});
+
+const QuantityComponent = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const { isDisplayOnly } = props;
+
+  const DisplayComponent = isDisplayOnly ? DefaultDisplay : AdjustmentDisplay;
+
+  return <DisplayComponent {...props} ref={ref} />;
 });
 
 export default QuantityComponent;
