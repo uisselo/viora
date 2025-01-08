d1a735772c518e63262f89b7cf6c4a6082e48b83
uisselo <louissemevale@gmail.com>
2025-01-08 21:06:11 +0800

    refactor(TextInputComponent): remove constant for classes and include input element attributes as props

--- src/components/common/TextInputComponent.tsx
+++ src/components/common/TextInputComponent.tsx
@@ -1,46 +1,48 @@
-import classNames from "classnames";
-import type { HeroIconProp } from "../../utils";
+import type { HTMLAttributes } from "react";
+import { cn, type HeroIconProp } from "@Utilities";
 
 type Props = {
   label?: string;
-  placeholder?: string;
   hideLabel?: boolean;
   icon?: HeroIconProp;
-};
+  placeholder?: string;
+} & HTMLAttributes<HTMLInputElement>;
 
 function TextInputComponent(props: Props) {
   const {
     label = "Label",
-    placeholder = "Placeholder",
     hideLabel,
     icon: Icon,
+    placeholder = "Placeholder",
+    className,
+    ...inputProps
   } = props;
 
-  const CLASSES = {
-    CONTAINER: classNames(
-      "relative flex items-center px-3 w-full bg-transparent text-sm md:text-base rounded border border-gray-400",
-      hideLabel ? "py-2" : "py-3",
-    ),
-    INPUT_BOX: "block w-full appearance-none focus:outline-none focus:ring-0",
-    LABEL:
-      "absolute top-2 z-10 text-gray-600 text-xs md:text-sm bg-white px-1 transform -translate-y-4 -translate-x-1 origin-[0]",
-    ICON: "w-4 h-4 md:w-5 md:h-5 text-gray-400 stroke-current stroke-2",
-  };
-
   return (
-    <div className={CLASSES.CONTAINER}>
+    <div
+      className={cn(
+        "relative flex items-center px-3 w-full bg-transparent text-sm md:text-base rounded border border-gray-400",
+        hideLabel ? "py-2" : "py-3",
+        className,
+      )}
+    >
       <input
-        id="input_box"
         type="text"
-        className={CLASSES.INPUT_BOX}
+        className="block w-full appearance-none focus:outline-none focus:ring-0"
         placeholder={placeholder}
+        {...inputProps}
       />
       {!hideLabel && (
-        <label htmlFor="input_box" className={CLASSES.LABEL}>
+        <label
+          htmlFor="input_box"
+          className="absolute top-2 z-10 text-gray-600 text-xs md:text-sm bg-white px-1 transform -translate-y-4 -translate-x-1 origin-[0]"
+        >
           {label}
         </label>
       )}
-      {Icon && <Icon className={CLASSES.ICON} />}
+      {Icon && (
+        <Icon className="text-gray-400 stroke-current stroke-2 size-4 md:size-5" />
+      )}
     </div>
   );
 }
