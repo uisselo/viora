8af507c227d24281a9d40e2a4563b17c8355e8d9
uisselo <louissemevale@gmail.com>
2025-01-08 20:55:57 +0800

    style: enhance tailwind config with custom container settings and breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
-			screens: {
-				"custom-lg": "1200px"
-			},
 			colors: {
 				primary: "#A45B94",
 				secondary: "#EDDEEA",
@@ -13,6 +10,23 @@ export default {
 		},
 		fontFamily: {
 			sans: ["Urbanist"],
+		},
+		container: {
+			center: true,
+			screens: {
+				sm: "100%",
+				md: "768px",
+				lg: "1024px",
+				xl: "1440px",
+			},
+			minWidth: "375px",
+			maxWidth: "1440px",
+			padding: {
+				DEFAULT: "16px",
+				sm: "16px",
+				md: "32px",
+				xl: "104px",
+			},
 		},
 	},
 	plugins: [],
