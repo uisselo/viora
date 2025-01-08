0c7c59757a37f2357827be6070707aebfb281c7a
uisselo <louissemevale@gmail.com>
2025-01-08 21:03:59 +0800

    refactor(ButtonIconComponent): simplified functionality

--- src/components/common/ButtonIconComponent.tsx
+++ src/components/common/ButtonIconComponent.tsx
@@ -1,61 +1,39 @@
-import classNames from "classnames";
-import type { HeroIconProp } from "../../utils";
+import type { ButtonHTMLAttributes, PropsWithChildren } from "react";
+import { cn, type HeroIconProp } from "@Utilities";
 
-type Props = {
-  display?: "header" | "default";
-  icon: HeroIconProp;
-  badgeValue?: string;
-  isBordered?: boolean;
-  size?: number;
-  onClick?: () => void;
-};
-
-function HeaderDisplay(props: Props) {
-  const { icon: Icon, badgeValue, onClick } = props;
-
-  return (
-    <button
-      className="relative flex items-center justify-center p-2 duration-300 ease-in-out bg-white rounded-full w-max hover:bg-gray-100 hover:delay-100"
-      onClick={onClick}
-    >
-      <Icon className="stroke-current stroke-1.5 w-7 h-7 text-primary" />
-      {badgeValue && (
-        <div className="absolute left-0 z-10 flex items-center justify-center w-auto h-auto px-1 translate-x-6 bg-red-500 rounded-full bottom-1 min-w-4">
-          <span className="text-xs font-medium text-white whitespace-nowrap">
-            {badgeValue}
-          </span>
-        </div>
-      )}
-    </button>
-  );
-}
-
-function DefaultDisplay(props: Props) {
-  const { icon: Icon, isBordered, size, onClick } = props;
-
-  return (
-    <button
-      style={{ width: `${size}px`, height: `${size}px` }}
-      className={classNames(
-        { "border border-gray-300": isBordered },
-        "flex items-center justify-center bg-white min-w-8 min-h-8 aspect-ratio",
-      )}
-      onClick={onClick}
-    >
-      <Icon className="stroke-current stroke-1.5 w-3.5 h-3.5 md:w-4 md:h-4" />
-    </button>
-  );
-}
+type Props = PropsWithChildren<
+  {
+    icon: HeroIconProp;
+    iconClassName?: string;
+    containerSize?: number;
+  } & ButtonHTMLAttributes<HTMLButtonElement>
+>;
 
 function ButtonIconComponent(props: Props) {
-  const MAP_DISPLAY = {
-    header: () => HeaderDisplay(props),
-    default: () => DefaultDisplay(props),
-  };
+  const {
+    children,
+    icon: Icon,
+    iconClassName,
+    containerSize,
+    className,
+    ...buttonProps
+  } = props;
 
-  const DisplayComponent = MAP_DISPLAY[props.display || "default"];
-
-  return <DisplayComponent />;
+  return (
+    <button
+      style={{ width: `${containerSize}px`, height: `${containerSize}px` }}
+      className={cn("relative flex-center p-2 w-max", className)}
+      {...buttonProps}
+    >
+      <Icon
+        className={cn(
+          "size-7 stroke-current stroke-1.5 stroke-primary",
+          iconClassName,
+        )}
+      />
+      {children}
+    </button>
+  );
 }
 
 export default ButtonIconComponent;
