83f0a4ec06d3189e06563f31225575966fd3b5e7
uisselo <louissemevale@gmail.com>
2025-01-08 20:54:20 +0800

    chore: installed clsx, swiper, tailwind-merge, and @types/node, and set up import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@Assets": "/src/assets",
+      "@GlobalComponents": "/src/components",
+      "@Modules": "/src/modules",
+      "@Pages": "/src/pages",
+      "@Store": "/src/store",
+      "@Utilities": "/src/utils",
+    },
+  },
+});
