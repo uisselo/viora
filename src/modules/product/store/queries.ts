0c30c868a270ad51a80a6035c2076c49afd2eb89
Louisse <louissevale@icloud.com>
2025-01-09 16:19:10 +0800

    feat(modules/product/store): create api.ts

--- src/modules/product/store/queries.ts
+++ src/modules/product/store/queries.ts
@@ -1,33 +1,33 @@
 import { useQuery } from "react-query";
-import type { ProductCategoryResponse, ProductItem } from "./types";
-
-const fetchProducts = async (category: string) => {
-  const response = await fetch(
-    `https://dummyjson.com/products/category/${category}`,
-  );
-  if (!response.ok) throw new Error("Error fetching products");
-  return response.json();
-};
-
-const fetchProduct = async (id: string) => {
-  const response = await fetch(`https://dummyjson.com/products/${id}`);
-  if (!response.ok) throw new Error("Error fetching product.");
-  return response.json();
-};
+import { ProductsApi } from "./api";
 
 export function useProductQueries(id?: string) {
-  const { isLoading: isBeautyDataLoading, data: beauty } =
-    useQuery<ProductCategoryResponse>("beauty", () => fetchProducts("beauty"));
+  const { isLoading: isBeautyProductsLoading, data: beauty } = useQuery(
+    "beauty",
+    () => ProductsApi.fetchProductsByCategory("beauty"),
+  );
 
-  const { isLoading: isProductLoading, data: productData } =
-    useQuery<ProductItem>(["product", id], () => fetchProduct(id!), {
-      enabled: !!id,
-    });
+  const { isLoading: isFragranceProductsLoading, data: fragrance } = useQuery(
+    "fragrance",
+    () => ProductsApi.fetchProductsByCategory("fragrance"),
+  );
+
+  const { isLoading: isProductDetailsLoading, data: productDetails } = useQuery(
+    ["product", id],
+    () => {
+      if (id) {
+        ProductsApi.fetchProductDetails(id);
+      }
+    },
+    { enabled: !!id },
+  );
 
   return {
-    isBeautyDataLoading,
-    beautyData: beauty?.products,
-    isProductLoading,
-    productData,
+    isBeautyProductsLoading,
+    beautyProducts: beauty?.products,
+    isFragranceProductsLoading,
+    fragranceProducts: fragrance?.products,
+    isProductDetailsLoading,
+    productDetails,
   };
 }
