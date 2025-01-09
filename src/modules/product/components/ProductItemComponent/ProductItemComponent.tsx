253a4986434f32fc2a92a17ce06fc1e8e6fea3d1
Louisse <louissevale@icloud.com>
2025-01-09 18:16:39 +0800

    refactor(ProductItemComponent): remove components folder

--- src/modules/product/components/ProductItemComponent/ProductItemComponent.tsx
+++ src/modules/product/components/ProductItemComponent/ProductItemComponent.tsx
@@ -1,12 +1,10 @@
-import type { PropsWithChildren } from "react";
+import type { HTMLAttributes, PropsWithChildren } from "react";
+import clsx from "clsx";
+import { cn } from "@Utilities";
 import type { ProductItem } from "../../store";
-import {
-  ProductImage,
-  ProductPrice,
-  ProductTitle,
-  ProductTotalPrice,
-} from "./components";
-import ProductItemContext from "./ProductItemContext";
+import ProductItemContext, {
+  useProductItemContext,
+} from "./ProductItemContext";
 
 type Props = PropsWithChildren<{ data: ProductItem; isProductPage?: boolean }>;
 
@@ -20,9 +18,66 @@ function ProductItemComponent(props: Props) {
   );
 }
 
-ProductItemComponent.Image = ProductImage;
-ProductItemComponent.Title = ProductTitle;
-ProductItemComponent.Price = ProductPrice;
-ProductItemComponent.TotalPrice = ProductTotalPrice;
+ProductItemComponent.Image = Image;
+ProductItemComponent.Title = Title;
+ProductItemComponent.Price = Price;
+ProductItemComponent.TotalPrice = TotalPrice;
 
 export default ProductItemComponent;
+
+function Image(props: HTMLAttributes<HTMLDivElement>) {
+  const { className, ...divProps } = props;
+  const { data } = useProductItemContext();
+
+  return (
+    <div
+      className={cn(
+        "w-full flex-center bg-white border-[1.5px] border-gray-300 aspect-square",
+        className,
+      )}
+      {...divProps}
+    >
+      <img src={data.thumbnail} alt="Product" />
+    </div>
+  );
+}
+
+function Title() {
+  const { data, isProductPage } = useProductItemContext();
+
+  return (
+    <p
+      className={clsx(
+        "font-semibold break-words line-clamp-2 w-full",
+        isProductPage ? "text-lg md:text-xl" : "text-sm md:text-base",
+      )}
+    >
+      {data.title}
+    </p>
+  );
+}
+
+function Price() {
+  const { data, isProductPage } = useProductItemContext();
+
+  return (
+    <p
+      className={clsx(
+        isProductPage
+          ? "font-semibold md:text-lg"
+          : "text-sm font-medium md:text-base",
+        "whitespace-nowrap",
+      )}
+    >
+      $ {data.price}
+    </p>
+  );
+}
+
+function TotalPrice({ value }: { value: number }) {
+  return (
+    <p className="text-sm font-semibold md:text-base whitespace-nowrap">
+      $ {value}
+    </p>
+  );
+}
--- src/modules/product/components/ProductItemComponent/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./ProductImage";
-export * from "./ProductPrice";
-export * from "./ProductTitle";
-export * from "./ProductTotalPrice";
