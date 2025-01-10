17956cb4b9ccf215e2cbb4ef2c92a2d5b2a3e2ff
uisselo <louissemevale@gmail.com>
2025-01-10 20:43:47 +0800

    feat(functions.ts): create formatDate util function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,5 +1,12 @@
 import clsx, { type ClassValue } from "clsx";
+import moment, { type Moment } from "moment";
 import { twMerge } from "tailwind-merge";
+
+function formatDate(date: string | Moment | Date, pattern?: string) {
+  return moment(date).format(pattern || "MMMM DD, YYYY");
+}
+
+export { formatDate };
 
 export function cn(...classes: ClassValue[]) {
   return twMerge(clsx(...classes));
