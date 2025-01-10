ce26c193596f5c0917b35cc372214616576dc316
uisselo <louissemevale@gmail.com>
2025-01-10 20:13:43 +0800

    feat(AccordionComponent): use Disclosure from headlessui

--- src/components/common/AccordionComponent.tsx
+++ src/components/common/AccordionComponent.tsx
@@ -1,40 +1,79 @@
+import type { HTMLAttributes, PropsWithChildren } from "react";
+import { ChevronRightIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Transition,
+} from "@headlessui/react";
 import { cn } from "@Utilities";
-import type {
-  ButtonHTMLAttributes,
-  HTMLAttributes,
-  PropsWithChildren,
-} from "react";
 
-function AccordionComponent({ children }: PropsWithChildren) {
-  return <div className="space-y-0.5">{children}</div>;
-}
-
-AccordionComponent.Heading = AccordionHeading;
-AccordionComponent.Body = AccordionBody;
-
-export default AccordionComponent;
-
-type AccordionHeadingProps = {
-  text?: string;
-} & ButtonHTMLAttributes<HTMLButtonElement>;
-type AccordionBodyProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>>;
-
-function AccordionHeading(props: AccordionHeadingProps) {
-  const { text = "Heading here", className, ...buttonProps } = props;
-
+function AccordionComponent({
+  children,
+  className,
+}: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
   return (
-    <button className={cn("p-4 bg-gray-50", className)} {...buttonProps}>
-      {text}
-    </button>
+    <Disclosure as="div" className={cn("space-y-0.5", className)}>
+      {children}
+    </Disclosure>
   );
 }
 
-function AccordionBody(props: AccordionBodyProps) {
-  const { children, className, ...divProps } = props;
+AccordionComponent.Button = Button;
+AccordionComponent.Panel = Panel;
 
+export default AccordionComponent;
+
+function Button({
+  children,
+  text = "Button text here",
+  className,
+}: PropsWithChildren<{ text?: string } & HTMLAttributes<HTMLButtonElement>>) {
   return (
-    <div className={cn("p-4 bg-gray-50", className)} {...divProps}>
-      {children}
-    </div>
+    <DisclosureButton
+      className={cn(
+        "flex items-center justify-between w-full p-3 font-medium border border-gray-300 md:p-4 bg-gray-50 md:text-lg",
+        className,
+      )}
+    >
+      {({ open }) => {
+        const Icon = open ? ChevronUpIcon : ChevronRightIcon;
+
+        return (
+          <>
+            {children || text}
+            <Icon className="size-4 md:size-5 stroke-current stroke-1.5" />
+          </>
+        );
+      }}
+    </DisclosureButton>
+  );
+}
+
+function Panel({
+  children,
+  text,
+  className,
+}: PropsWithChildren<{ text?: string } & HTMLAttributes<HTMLDivElement>>) {
+  return (
+    <Transition
+      as="div"
+      className="overflow-hidden origin-top"
+      enter="transition-all duration-300 ease-out"
+      enterFrom="transform scale-y-0 opacity-0 max-h-0"
+      enterTo="transform scale-y-100 opacity-100 max-h-screen"
+      leave="transition-all duration-200 ease-in"
+      leaveFrom="transform scale-y-100 opacity-100 max-h-screen"
+      leaveTo="transform scale-y-0 opacity-0 max-h-0"
+    >
+      <DisclosurePanel
+        className={cn(
+          "p-3 text-sm border border-gray-300 md:p-4 md:text-base",
+          className,
+        )}
+      >
+        {children || text}
+      </DisclosurePanel>
+    </Transition>
   );
 }
