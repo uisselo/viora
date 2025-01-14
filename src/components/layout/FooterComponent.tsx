f963bc2e167a611f8e9d380519d0ded79de96f80
uisselo <louissemevale@gmail.com>
2025-01-15 01:19:45 +0800

    feat(FooterComponent): add a disclaimer, and buttons for the website's GitHub link and components page

--- src/components/layout/FooterComponent.tsx
+++ src/components/layout/FooterComponent.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { useWindowSize } from "@uidotdev/usehooks";
 import { SVGs } from "@Assets";
 import { FOOTER_NAV_ITEMS } from "@Config";
@@ -31,10 +32,31 @@ function FooterComponent() {
   return (
     <footer className="w-full border-t">
       <div className="container flex flex-col gap-8 py-4 md:grid md:grid-cols-12 md:py-8 lg:gap-5">
-        <div className="md:col-span-6">
+        <div className="flex flex-col gap-4 md:col-span-6">
           <Link to="/">
             <img src={SVGs.viora_logo} alt="Viora Logo" className="w-fit" />
           </Link>
+          <p className="lg:w-[30rem] text-xs md:text-sm font-medium">
+            This website is not fully functional. This is a portfolio project
+            created to showcase design and development skills.
+          </p>
+          {width && (
+            <div className="space-y-1">
+              <ButtonComponent
+                text="View on GitHub"
+                variant="link"
+                size={width > 768 ? "sm" : "xs"}
+                icon={ArrowRightIcon}
+              />
+              <ButtonComponent
+                text="View Components here"
+                variant="link"
+                navigateTo="/components"
+                size={width > 768 ? "sm" : "xs"}
+                icon={ArrowRightIcon}
+              />
+            </div>
+          )}
         </div>
         <div className="flex flex-col gap-4 md:grid md:grid-cols-4 lg:gap-5 md:col-span-6">
           {FOOTER_NAV_ITEMS.map(({ section, items }) =>
