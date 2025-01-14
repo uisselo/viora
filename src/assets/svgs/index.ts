337dc83144efbd19d045f1dc3e8e2dd7b5a21e78
uisselo <louissemevale@gmail.com>
2025-01-15 01:14:32 +0800

    feat(assets): import svg assets for error page

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,17 @@
+import error_404_brush from "./error_404_brush.svg";
+import error_404_lipstick from "./error_404_lipstick.svg";
+import error_404_mascara from "./error_404_mascara.svg";
+import error_404_nail_polish from "./error_404_nail_polish.svg";
+import error_404_palette from "./error_404_palette.svg";
+import error_404_text from "./error_404_text.svg";
 import viora_logo from "./viora_logo.svg";
 
-export const SVGs = { viora_logo };
+export const SVGs = {
+  error_404_brush,
+  error_404_lipstick,
+  error_404_mascara,
+  error_404_nail_polish,
+  error_404_palette,
+  error_404_text,
+  viora_logo,
+};
